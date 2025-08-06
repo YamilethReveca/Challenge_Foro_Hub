0e011b90adf4ff7a5f58d068ae40e7be4c8504dd
yamileth caldera <yamilethcaldera@gmail.com>
2025-08-06 19:07:44 -0400

    Iniciando desafio

---
+++ src/main/java/com/alura/Foro/Hub/challenge/ForoHubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alura.Foro.Hub.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/Foro/Hub/challenge/ForoHubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.Foro.Hub.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
